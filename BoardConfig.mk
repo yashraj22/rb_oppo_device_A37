ef2356fde86459f3e98eb30764848824141c7b4b
Ricky Cheung <rcheung844@gmail.com>
2022-03-08 17:14:11 +0800

    A37: DOn't override TARGET_LD_SHIM_LIBS
    * We need to get common tree shims to load
    
    Change-Id: I1084655c54e45ea533454b67e2a1a40f27e99145

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -189,8 +189,8 @@ TARGET_NO_RPC := true
 USE_DEVICE_SPECIFIC_GPS := true
 SELINUX_IGNORE_NEVERALLOWS := true
 
-# Shim
-TARGET_LD_SHIM_LIBS := \
+# Shims
+TARGET_LD_SHIM_LIBS += \
     /system/vendor/lib/libmmcamera2_stats_modules.so|libshim_camera.so \
     /system/vendor/lib/libmmcamera2_stats_algorithm.so|libcamera_shim.so \
     /system/vendor/lib/hw/camera.vendor.msm8916.so|libshim_camera.so
